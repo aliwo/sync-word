5af5ae6b67f84f5f32aac8d511944bc86c5c1008
aliwo <aliwo@naver.com>
2018-05-19 10:58:24 +0900

    레플 입사문제 코

--- app/main/events.py
+++ app/main/events.py
@@ -19,6 +19,28 @@ def text(message):
     room = session.get('room')
     emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
 
+@socketio.on('word_enter', namespace='/chat')
+def word(message):
+    """Sent by a client when the user entered a new message.
+    The message is sent to all people in the room."""
+    room = session.get('room')
+    emit('word_message', {'msg':message['msg']+ '\n'}, room=room)
+
+@socketio.on('word_space', namespace='/chat')
+def word_space(message):
+    """Sent by a client when the user entered a new message.
+    The message is sent to all people in the room."""
+    room = session.get('room')
+    emit('word_message', {'msg':message['msg']+ ' '}, room=room)
+
+@socketio.on('word_backspace', namespace='/chat')
+def word_backspace(message):
+    """Sent by a client when the user entered a new message.
+    The message is sent to all people in the room."""
+    room = session.get('room')
+    emit('word_message', {'msg':message['msg'][:-1]}, room=room)
+
+
 
 @socketio.on('left', namespace='/chat')
 def left(message):
